87669b7ea4aa590bc459fdd0f83942f97535befb
Gabriel Sabino <gabrielsabinopinho@gmail.com>
2025-06-05 17:11:46 -0300

    feat: melhorias css e javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,8 @@
+// javascript/script.js
 $(document).ready(function() {
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
+        // Opcional: Mudar o ícone do botão hamburguer para um "X" quando o menu está ativo
+        $(this).find('i').toggleClass('fa-bars fa-times');
     });
 });
