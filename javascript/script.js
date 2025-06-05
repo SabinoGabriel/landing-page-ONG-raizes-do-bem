1f478ad39d4bc87a927da720d09f13fdd3545e65
JPlimajots <jplimaano@gmail.com>
2025-06-05 19:02:49 -0300

    ajustes no header e resposividade mobile

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,18 @@
 // javascript/script.js
 $(document).ready(function() {
+    // Menu mobile toggle
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
         // Opcional: Mudar o ícone do botão hamburguer para um "X" quando o menu está ativo
         $(this).find('i').toggleClass('fa-bars fa-times');
     });
+    // Fechar menu ao clicar em um link
+    $('.nav_item a').on('click', function() {
+        if($('#mobile_menu').hasClass('active')) {
+            $('#mobile_menu').removeClass('active');
+            $('#mobile_btn').find('i').removeClass('fa-times').addClass('fa-bars');
+        }
+    });
+    
 });
+
